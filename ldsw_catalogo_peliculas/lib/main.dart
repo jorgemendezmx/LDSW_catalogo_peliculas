1bd49d10c55cdb6ba2eca949300fc543677f237e
Jorge Mendez Marin <jamendez13@outlook.com>
2026-05-14 23:25:01 -0600

    Instalación de Firebase

--- ldsw_catalogo_peliculas/lib/main.dart
+++ ldsw_catalogo_peliculas/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'dart:convert';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // Widget central de la aplicación, sin estado (StatelessWidget)
   // Define la estructura básica de la aplicación, incluyendo el tema y la página de inicio.
